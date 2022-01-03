fa03d365d6baf86bedce895371f7587a4d7a0189
j624364 <49117678+j624364@users.noreply.github.com>
2022-01-03 20:44:05 +0000

    Add extra aliases

--- config.fish
+++ config.fish
@@ -3,5 +3,11 @@ end
 
 if status is-interactive
 	alias l="ls -a --color"
+	alias gs="git status"
+	alias gd="git diff"
+	alias ga="git add"
+	alias gc="git commit"
+	alias so="source ~/.config/fish/config.fish"
+
 	set -gx PATH $PATH ~/opt/cross/bin
 end
